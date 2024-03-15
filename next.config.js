86bca5cdc7822727695e93436816cafffa073ae0
Marcos Soledade <marcosoledadejr@gmail.com>
2024-03-15 16:44:16 -0300

    Ignores linting on build

--- next.config.js
+++ next.config.js
@@ -4,18 +4,23 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/campaigns',
+        source: "/",
+        destination: "/campaigns",
         basePath: false,
-        permanent: false
-      }
-    ]
+        permanent: false,
+      },
+    ];
   },
   i18n: {
-    locales: ['en-US', 'es'],
-    defaultLocale: 'en-US',
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
     // localePath: resolve('./public/locales'),
-  }
-}
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
